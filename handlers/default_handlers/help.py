8c1b5da93eacb15058cf213669e6708fa595a861
Petro Saprykin <petrosaprikinloh@gmail.com>
2023-09-12 17:30:21 +0300

    finished working on the help command

--- handlers/default_handlers/help.py
+++ handlers/default_handlers/help.py
@@ -1,9 +1,12 @@
 from telebot.types import Message
 
 from loader import bot
-from keyboards.reply.command_list import commands_markup
+from config_data.config import DEFAULT_COMMANDS
 
 
 @bot.message_handler(commands=['help'])
 def bot_start(message: Message):
-    bot.send_message(message.chat.id, "Виберіть потрібну команду:", reply_markup=commands_markup())
+    text = ""
+    for command, description in DEFAULT_COMMANDS:
+        text += f"{description}: /{command}\n"
+    bot.send_message(message.chat.id, f"Виберіть потрібну команду:\n{text}")
